315ff01724fe442a93c1e299ca4e29c366e64dd8
Ana Cabrera Agudelo <anacabreraagudelo@Anas-iMac.local>
2016-07-14 08:00:07 -0500

    Test

---
+++ MuseumApp/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  MuseumApp
+//
+//  Created by Ana Cabrera Agudelo on 7/12/16.
+//  Copyright © 2016 Ana Cabrera Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
